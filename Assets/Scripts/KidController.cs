2eadc396ae07550090a5318b6b82b97072f4e793
Morgan <morgangb@googlemail.com>
2020-11-02 13:26:20 +0000

    my mouse stopped working
    oof

--- Assets/Scripts/KidController.cs
+++ Assets/Scripts/KidController.cs
@@ -8,9 +8,11 @@ public class KidController : MonoBehaviour
     [SerializeField] private GameObject myRobot;
     [SerializeField] private Camera myCamera;
     [SerializeField] private AudioListener myListener;
+    [SerializeField] private GameObject marker;
     
     private bool isRobot; //Is the player controlling the robot rn?
     private TurdmonkeysFirstPersonController myController;
+    private GameObject myMarker;
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +23,26 @@ public class KidController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isRobot) {
+
+        }
+        else {
+            //Make Marker
+            if (Input.GetButtonDown("Switch")) {
+                RaycastHit hit;
+                Ray ray = new Ray (myCamera.transform.position, transform.forward);
+                Physics.Raycast(ray, out hit, 5f);
+                myMarker = Instantiate(marker, hit.point);
+            }
+            //Marker
+            else if (Input.GetButton("Switch")) {
+
+            }
+        }
+
+        
         //Toggle isrobot
-        if (Input.GetButtonDown("Switch")) {
+        if (Input.GetButtonUp("Switch")) {
             isRobot = !isRobot;
         }
 
