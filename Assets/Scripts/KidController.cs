fd0dd052166cf9e526085c12083b6bcaac88d7df
Morgan <morgangb@googlemail.com>
2020-11-02 12:54:44 +0000

    Robot switching
    player may now switch from kid to robot and back again

---
+++ Assets/Scripts/KidController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class KidController : MonoBehaviour
+{
+    [SerializeField] private GameObject myRobot;
+    [SerializeField] private Camera myCamera;
+    [SerializeField] private AudioListener myListener;
+    
+    private bool isRobot; //Is the player controlling the robot rn?
+    private TurdmonkeysFirstPersonController myController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myController = GetComponent<TurdmonkeysFirstPersonController>(); //get the fps cont
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Toggle isrobot
+        if (Input.GetButtonDown("Switch")) {
+            isRobot = !isRobot;
+        }
+
+        //set active based on isrobot
+        myRobot.SetActive(isRobot);
+        myListener.enabled = !isRobot;
+        myCamera.enabled = !isRobot;
+        myController.enabled = !isRobot;
+    }
+}
