f6d39fd7415817e9e02ec960c31a3d9170622158
Cameron <alodith15@gmail.com>
2020-11-03 22:09:53 +0000

    Set Name 2.0 Fuckers
    WOO

---
+++ Assets/Scripts/PlayerName.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerName : MonoBehaviour
+{
+
+    public InputField nametf;
+    public Button setNameBtn;
+
+    public void OnTFChange(string value)
+    {
+        if(value.Length > 3)
+        {
+            setNameBtn.interactable = true;
+        }
+    }
+
+    public void OnClick_SetName()
+    {
+        PhotonNetwork.NickName = nametf.text;
+    }
+}
